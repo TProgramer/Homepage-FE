f5ca395131f6fab83a9867f2694f5e391969c37a
CWCTBOY <qkrdlswo98@gmail.com>
2022-04-10 05:57:24 +0900

    모달 이미지연동 및 애니메이션 수정했습니당 #16 closed

---
+++ src/styles/Activity/activity.tsx
@@ -0,0 +1,65 @@
+import styled from "@emotion/styled";
+export const Wrapper2 = styled('div')`
+  width: 100vw;
+  min-width: 550px;
+  min-height: 100vh;
+  height: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`//나중에 _app.tsx 기본 레이아웃 요구사항
+export const Masonry = styled('div')`
+  width: 800px;
+  padding: 10px;
+  display: grid;
+  grid-template-rows: repeat(3,210px);
+  grid-template-columns: repeat(3,1fr);
+  grid-auto-rows: 100px;
+  @media(max-width: 850px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`
+export const Block = styled('div')`
+  margin: 5px;
+  border-radius: 10px;
+  background-position: center;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  color: rgba(0,0,0,0);
+  box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;
+  transition: all 0.2s linear;
+  &:hover {
+  cursor: pointer;
+  opacity: 0.7;
+  color: whitesmoke;
+  &:nth-of-type(4) {
+    color: #04E148;
+  }
+}
+  &:nth-of-type(1) {
+    grid-column: span 2;
+  }
+  &:nth-of-type(7) {
+    grid-column: span 2;
+  }
+  @media(max-width:850px) {
+    width: 400px;
+    height: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  `
+export const Img = styled('img')`
+  width: 150%;
+  `
+export const Description = styled('h2')`
+  position: absolute;
+  font-size: 27px;
+  `
