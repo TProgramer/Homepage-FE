7b57b592892f78f39961b7abbbac4f455d113017
Seheon Yu <seheon@seoultech.ac.kr>
2022-05-17 20:14:50 +0900

    리더보드 API 작성

---
+++ src/pages/api/users/leaderboard.tsx
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getRandomUser } from ".";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method == "GET") {
+    const { type } = req.query;
+    if (type !== "baekjoon" && type !== "attendance") {
+      res.status(400).end();
+    }
+    res.status(200).json(getRandomUser(3));
+  } else {
+    res.status(405).end();
+  }
+}
