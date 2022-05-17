2e06a35a7a5c2e979160295b49032b46189c7b30
Seheon Yu <seheon@seoultech.ac.kr>
2022-05-17 20:16:38 +0900

    leaderboard 디렉토리 수정

--- src/pages/api/leaderboard.tsx
+++ src/pages/api/leaderboard.tsx
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getRandomUser } from ".";
+import { getRandomUser } from "./users";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method == "GET") {
