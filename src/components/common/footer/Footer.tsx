62ab28ca7698fb9fb8d6a786688722b2355ea283
Seheon Yu <seheon@seoultech.ac.kr>
2022-06-19 18:05:38 +0900

    fix: Align infos

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -7,23 +7,25 @@ import { grey } from "@mui/material/colors";
 const Footer = () => {
   return (
     <StyledFooter>
-      <div className="inner_footer">
+      <div className="contents">
         <div className="title">
           <div className="NL">NL</div>
           <div className="NL_detail">Network Leader</div>
         </div>
-        <div className="subtitle">
-          <div className="info1">
+        <div className="about-us">
+          <div className="info-box">
             <MyLocationIcon sx={{ color: grey[50] }} fontSize="small" />
-            <span>서울특별시 노원구 공릉로 232 미래관 123호</span>
+            <span className="info-text">
+              서울특별시 노원구 공릉로 232 미래관 123호
+            </span>
           </div>
-          <div className="info2">
+          <div className="info-box">
             <AlternateEmailIcon sx={{ color: grey[50] }} fontSize="small" />
-            <span>newdeal123@nate.com</span>
+            <span className="info-text">newdeal123@nate.com</span>
           </div>
-          <div className="info3">
+          <div className="info-box">
             <GitHubIcon sx={{ color: grey[50] }} fontSize="small" />
-            <span>@Network-Leader</span>
+            <span className="info-text">@Network-Leader</span>
           </div>
         </div>
       </div>
--- src/styles/layout/main/footer.tsx
+++ src/styles/layout/main/footer.tsx
@@ -1,5 +1,10 @@
 import styled from "@emotion/styled";
-import { MOBILE_STANDARD, MAX_WIDTH } from "../../global/Global";
+import {
+  MOBILE_STANDARD,
+  MAX_WIDTH,
+  FONT_XXXL,
+  FONT_L,
+} from "../../global/Global";
 export const StyledFooter = styled("footer")`
   @media (max-width: ${MOBILE_STANDARD}px) {
     display: none;
@@ -8,59 +13,53 @@ export const StyledFooter = styled("footer")`
   height: 500px;
 
   width: 100%;
-  .inner_footer {
+  .contents {
     width: ${MAX_WIDTH};
     height: 100%;
     margin: 0 auto;
-    .title {
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      justify-content: flex-end;
-      height: 200px;
-      border-bottom: 1px solid #ffffff;
-      .NL {
-        font-size: 72px;
-        color: white;
-      }
-      .NL_detail {
-        font-size: 18px;
-        color: white;
-      }
+
+    display: flex;
+    flex-direction: column;
+  }
+
+  .title {
+    width: 100%;
+    height: 200px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+
+    border-bottom: 1px solid #ffffff;
+    padding-bottom: 30px;
+
+    .NL {
+      font-size: ${FONT_XXXL};
+      color: white;
     }
-    .subtitle {
-      position: relative;
-      width: 100%;
-      height: 300px;
-      .info1 {
-        position: absolute;
-        right: 0;
-        span {
-          font-size: 14px;
-          margin: 0 0 0 8px;
-          color: white;
-        }
-      }
-      .info2 {
-        position: absolute;
-        right: 0;
-        margin: 30px 101px 0 0;
-        span {
-          font-size: 14px;
-          margin: 0 0 0 8px;
-          color: white;
-        }
-      }
-      .info3 {
-        position: absolute;
-        right: 0;
-        margin: 60px 134px 0 0;
-        span {
-          font-size: 14px;
-          margin: 0 0 0 8px;
-          color: white;
-        }
-      }
+    .NL_detail {
+      font-size: ${FONT_L};
+      color: white;
     }
+  }
+
+  .about-us {
+    display: flex
+    flex-direction: column;
+
+    width: fit-content;
+    align-self: flex-end;
+
+    margin-top: 30px;
+  }
+
+  .info-box {
+    width: fit-content;
+  }
+
+  .info-text {
+    font-size: 14px;
+    margin: 0 0 0 8px;
+    color: white;
   }
 `;
