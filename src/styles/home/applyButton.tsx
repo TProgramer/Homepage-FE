8497e855ad5ddf9a7f33710b32e80588f89d8287
Seheon Yu <seheon@seoultech.ac.kr>
2022-06-19 17:05:55 +0900

    update: Add animation effect

--- src/styles/home/applyButton.tsx
+++ src/styles/home/applyButton.tsx
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import { FONT_XL, MAX_WIDTH } from "../global/Global";
+import { DEFAULT_BORDERRADIUS, FONT_XL, MAX_WIDTH } from "../global/Global";
 
 export const ApplyBox = styled("div")<{ backgroundImageUrl: string }>`
   width: ${MAX_WIDTH};
@@ -27,9 +27,20 @@ export const Apply = styled("a")`
   align-self: center;
   text-align: center;
 
+  border: 1px solid #000;
+  border-radius: 5px;
+
   font-size: ${FONT_XL};
 
   display: flex;
   align-items: center;
   justify-content: center;
+
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    cursor: pointer;
+    background-color: #000;
+    color: #fff;
+  }
 `;
