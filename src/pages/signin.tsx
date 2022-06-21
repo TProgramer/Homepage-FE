f9f035abfd2621b7522d2ffb17c3c7e28d954eff
rlarlduf20 <kky8274@naver.com>
2022-06-21 18:00:32 +0900

    로그인 페이지 useform 적용

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,11 +1,46 @@
+import { useState } from "react";
 import { Form } from "../styles/layout/sign/globalSignBox";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { ISignType } from "../components/common/sign/SignLayout";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 function SignIn() {
+  const [show, setShow] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ISignType>();
+  const onSubmit = (data: ISignType) => console.log(data);
   return (
-    <Form>
-      <input type="text" className="input" placeholder="Student ID" />
-      <input type="text" className="input" placeholder="Password" />
+    <Form onSubmit={handleSubmit(onSubmit)} show={show}>
+      <input
+        {...register("studentId", { required: true })}
+        type="text"
+        className="input"
+        placeholder="Student ID"
+      />
+      <div className="pwVisible">
+        <input
+          {...register("password", { required: true })}
+          type={!show ? "password" : "text"}
+          className="input"
+          placeholder="Password"
+        />
+        {!show ? (
+          <VisibilityIcon
+            className="visibleIcon"
+            onClick={() => setShow((prev) => !prev)}
+          />
+        ) : (
+          <VisibilityOffIcon
+            className="visibleIcon"
+            onClick={() => setShow((prev) => !prev)}
+          />
+        )}
+      </div>
       <button className="submit">LOG IN</button>
       <p className="toregister">
         <Link href="/signup">
