99f5d90fe46a105f3eef0e025b4a287924b32f0b
CWCTBOY <qkrdlswo98@gmail.com>
2022-06-23 19:21:23 +0900

    feat:정보수정form제외 myPage완성

--- src/components/myPage/Profile.tsx
+++ src/components/myPage/Profile.tsx
@@ -1,162 +1,121 @@
-import React, { useState } from 'react';
-import CreateIcon from '@mui/icons-material/Create';
-import CheckIcon from '@mui/icons-material/Check';
-import AddBoxIcon from '@mui/icons-material/AddBox';
-import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
-import CallOutlinedIcon from '@mui/icons-material/CallOutlined';
-import InsertLinkOutlinedIcon from '@mui/icons-material/InsertLinkOutlined';
-import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
+import React, { useState } from "react";
+import CreateIcon from "@mui/icons-material/Create";
+import CheckIcon from "@mui/icons-material/Check";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import CallOutlinedIcon from "@mui/icons-material/CallOutlined";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import InsertLinkOutlinedIcon from "@mui/icons-material/InsertLinkOutlined";
+import DoubleArrowIcon from "@mui/icons-material/DoubleArrow";
 import {
-  ProfileBox,
-  Profile,
-  ProfileImg,
-  TypeText,
-  Information,
-  ProfileHeader,
-  Name,
-  Info,
-  NameBox,
-  ProfileImg_unset,
+  Another,
+  Arrow,
+  Avatar,
   Bio,
-  Contact,
-  Input,
-  BioInput,
-  ContactBox,
-  Button
-} from '../../styles/myPage/header';
+  Contents,
+  ProfileContainer,
+} from "../../styles/myPage/myPage";
 
 interface IPersonalType {
-  readonly id: number;
   readonly name: string;
   readonly major: string;
-  type: 'FrontEnd' | 'BackEnd' | 'DevOps'
   grade: number;
+  type: "FrontEnd" | "BackEnd" | "DevOps";
   profileImg: string;
+  email: string;
+  number: string;
+  github: string;
+  link: string;
 }
-const personalData: IPersonalType[] = [
-  {
-    id: 0,
-    name: '박인재',
-    major: '건축공학과',
-    type: 'FrontEnd',
-    grade: 2,
-    profileImg: '',//https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzdkVemYyTTjjwW8ax1aJ-kHvwR6aK1VTPKRSbvlnIlMs340p0iFzcs_u878365DmbZvI&usqp=CAU
-  },
-]
-const iconStyle: {
-  fontSize: number;
-  marginRight: number;
-} = {
-  fontSize: 20,
-  marginRight: 1
-};
 
 const MyProfile = () => {
-  const [onoff, setOnoff] = useState<boolean>(false);
-  const onClick = () => {
-    setOnoff(!onoff);
+  const [myForm, setMyForm] = useState(false);
+  const [move, setMove] = useState(225);
+  const [personalData, setPersonalData] = useState<IPersonalType>({
+    name: "박인재",
+    major: "건축공학과",
+    grade: 2,
+    type: "FrontEnd",
+    profileImg: "",
+    // "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzdkVemYyTTjjwW8ax1aJ-kHvwR6aK1VTPKRSbvlnIlMs340p0iFzcs_u878365DmbZvI&usqp=CAU",
+    email: "qkrdlswo98@naver.com",
+    number: "010-3393-9410",
+    github: "https://CWCTBOY.github.com",
+    link: "https://wakatime.com",
+  });
+  const onClickLeft = () => {
+    setMove((prev) => prev + 450);
   };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  }
-  const [bio, setBio] = useState<string>('This is Bio Container\nThis is Bio Container\nThis is Bio Container\nThis is Bio Container\nThis is Bio Container');
-  const [mail, setMail] = useState<string>('qkrdlswo98@gmail.com');
-  const [number, setNumber] = useState<string>('010-3393-9410');
-  const [link, setLink] = useState<string>('https://wakatime.com/dashboard');
-  const [team, setTeam] = useState<string>('NL Homepage-FE Team');
-  const changeBio = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setBio(e.target.value);
+  const onClickRight = () => {
+    setMove((prev) => prev - 450);
   };
-  const changeMail = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setMail(e.target.value);
-  };
-  const changeNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNumber(e.target.value);
-  };
-  const changeLink = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLink(e.target.value);
-  };
-  const changeTeam = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTeam(e.target.value);
-  };
+  console.log(myForm);
   return (
-    <>
-      <ProfileBox onoff={onoff}>
-        <Profile>
-          {
-            onoff
-              ? <ProfileImg_unset><AddBoxIcon sx={{ fontSize: 35, color: 'black', opacity: 0.3, cursor: 'pointer' }} /></ProfileImg_unset>
-              : (personalData[0].profileImg !== ''
-                ? <ProfileImg src={personalData[0].profileImg} />
-                : <ProfileImg_unset>Add your Avatar</ProfileImg_unset>)
-          }
-          <TypeText>{`<\ ${personalData[0]?.type} / >`}</TypeText>
-        </Profile>
-        <Information>
-          <ProfileHeader>
-            <NameBox>
-              <Name>{personalData[0].name}</Name>
-              <Info>{`${personalData[0].major} ${personalData[0].grade}학년`}</Info>
-            </NameBox>
-          </ProfileHeader>
-          <ContactBox onSubmit={onSubmit}>
-            <Bio>
-              {
-                onoff
-                  ? (
-                    <BioInput type='textarea' onChange={changeBio} value={bio} />
-                  )
-                  : bio
-              }
-            </Bio>
-            <Contact><EmailOutlinedIcon sx={iconStyle} />
-              {
-                onoff
-                  ? (
-                    <Input type='email' onChange={changeMail} value={mail} />
-                  )
-                  : mail
-              }
-            </Contact>
-            <Contact><CallOutlinedIcon sx={iconStyle} />
-              {
-                onoff
-                  ? (
-                    <Input type='text' onChange={changeNumber} value={number} />
-                  )
-                  : number
-              }
-            </Contact>
-            <Contact><InsertLinkOutlinedIcon sx={iconStyle} />
-              {
-                onoff
-                  ? (
-                    <Input type='url' onChange={changeLink} value={link} />
-                  )
-                  : <a href={link} target='_blank'>{link}</a>
-              }
-            </Contact>
-            <Contact><GroupsOutlinedIcon sx={iconStyle} />
-              {
-                onoff
-                  ? (
-                    <Input type='text' onChange={changeTeam} value={team} />
-                  )
-                  : team
-              }
-            </Contact>
-            <Button
-              onClick={onClick}
-              type={!onoff ? 'button' : 'submit'}
-            >
-              {
-                !onoff ? <CreateIcon /> : <CheckIcon />
-              }
-            </Button>
-          </ContactBox>
-        </Information>
-      </ProfileBox >
-    </>
-  )
-}
+    <ProfileContainer myForm={myForm}>
+      <Avatar>
+        <div
+          className="edit"
+          onClick={() => {
+            setMyForm(true);
+          }}
+        >
+          <CreateIcon />
+        </div>
+        <div className="avatar">
+          {personalData.profileImg === "" ? (
+            "Add your Avatar"
+          ) : (
+            <img
+              src={personalData.profileImg}
+              alt="avatar"
+              className="avatar__img"
+            />
+          )}
+        </div>
+        <div className="info__header">
+          <h2 className="name">{personalData.name}</h2>
+          <h3 className="major--grade">{`${personalData.major} ${personalData.grade}학년`}</h3>
+        </div>
+        <span className="type">{`<${personalData.type}/>`}</span>
+      </Avatar>
+      <Contents move={move}>
+        <Bio>
+          <p className="paragraph">
+            안녕하세요 저는 건축공학과 2학년 재학중인 박인재입니다 안녕하세요
+            저는 건축공학과 2학년 재학중인 박인재입니다 안녕하세요 저는
+            건축공학과 2학년 재학중인 박인재입니다 안녕하세요 저는 건축공학과
+            저는 건축공학과 2학년 재학중인 박인재입니다 안녕하세요 저는
+            건축공학과 2학년 재학중인 박인재입니
+          </p>
+        </Bio>
+        <Another>
+          <div className="infoBox">
+            <EmailOutlinedIcon />
+            <span className="info">{personalData.email}</span>
+          </div>
+          <div className="infoBox">
+            <CallOutlinedIcon />
+            <span className="info">{personalData.number}</span>
+          </div>
+          <div className="infoBox">
+            <GitHubIcon />
+            <span className="info">{personalData.github}</span>
+          </div>
+          <div className="infoBox">
+            <InsertLinkOutlinedIcon />
+            <span className="info">{personalData.link}</span>
+          </div>
+        </Another>
+      </Contents>
+      <Arrow>
+        {move === 225 ? (
+          <DoubleArrowIcon className="arrow arrow__r" onClick={onClickRight} />
+        ) : (
+          <DoubleArrowIcon className="arrow arrow__l" onClick={onClickLeft} />
+        )}
+      </Arrow>
+      <ProfileContainer className="form"></ProfileContainer>
+    </ProfileContainer>
+  );
+};
 export default MyProfile;
--- src/styles/myPage/myPage.tsx
+++ src/styles/myPage/myPage.tsx
@@ -1,11 +1,171 @@
 import styled from "@emotion/styled";
+import {
+  DEFAULT_BORDERRADIUS,
+  DEFAULT_BOXSHADOW,
+  FONTWEIGHT_BOLD,
+  FONT_L,
+  FONT_XL,
+  FONT_XXL,
+  TRANSITION,
+} from "../global/Global";
 
-export const Wrapper = styled('div')`
+export const Wrapper = styled("div")`
   width: 100vw;
-  min-width: 550px;
   min-height: 100vh;
   height: auto;
   display: flex;
-  justify-content: center;
+`;
+
+export const ProfileContainer = styled("div")<{
+  myForm?: boolean;
+}>`
+  width: 450px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
   align-items: center;
-`//아직 레이아웃이 적용되지 않아 임시로 만듬
+  margin: 100px 0 0 0;
+  position: relative;
+  ${DEFAULT_BOXSHADOW}
+  overflow: hidden;
+  .form {
+    width: 450px;
+    height: 100vh;
+    display: flex;
+    position: absolute;
+    top: -100px;
+    flex-direction: column;
+    align-items: center;
+    margin: 100px 0 0 0;
+    background-color: rgba(0, 0, 0, 0.3);
+    ${TRANSITION}
+    transform: translateX(${(props) => (props.myForm ? 0 : "450px")});
+    box-shadow: none;
+    z-index: 2;
+  }
+`;
+
+export const Avatar = styled("div")`
+  width: 100%;
+  height: 420px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 20px;
+  .avatar {
+    width: 320px;
+    height: 320px;
+    background-color: white;
+    border-radius: 50%;
+    overflow: hidden;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 0 10px 0;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset,
+      rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
+    .avatar__img {
+      width: 100%;
+    }
+    .addIcon {
+      font-size: ${FONT_XXL};
+      color: rgba(0, 0, 0, 0.3);
+    }
+  }
+  .edit {
+    width: 50px;
+    height: 50px;
+    position: absolute;
+    top: 34%;
+    left: 70%;
+    z-index: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    ${DEFAULT_BOXSHADOW}
+    color: ${(props) => props.theme.textColor.sub};
+    background-color: ${(props) => props.theme.buttonColor.primary};
+    ${TRANSITION}
+    &:hover {
+      background-color: ${(props) => props.theme.buttonColor.onHover};
+    }
+  }
+  .info__header {
+    width: auto;
+    display: flex;
+    align-items: flex-end;
+    margin-bottom: 10px;
+    .name {
+      font-size: ${FONT_XL};
+      color: ${(props) => props.theme.textColor.primary};
+      font-weight: ${FONTWEIGHT_BOLD};
+      margin-right: 5px;
+    }
+    .major--grade {
+      font-size: ${FONT_L};
+      color: rgba(0, 0, 0, 0.4);
+    }
+  }
+  .type {
+    font-size: ${FONT_XL};
+    color: ${(props) => props.theme.buttonColor.primary};
+  }
+`;
+export const Contents = styled("div")<{ move: number }>`
+  width: 900px;
+  min-height: 160px;
+  display: flex;
+  ${TRANSITION}
+  transform: translateX(${(props) => `${props.move}px`});
+`;
+export const Bio = styled("div")`
+  width: 450px;
+  height: auto;
+  display: flex;
+  justify-content: center;
+  margin: 20px 0;
+  .paragraph {
+    white-space: normal;
+    word-break: break-all;
+    width: 350px;
+    height: auto;
+    font-size: ${FONT_L};
+    color: rgba(0, 0, 0, 0.4);
+  }
+`;
+export const Another = styled("div")`
+  width: 450px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  .infoBox {
+    width: fit-content;
+    height: 30px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin: 0 0 10px 90px;
+    color: rgba(0, 0, 0, 0.4);
+    .info {
+      margin: 0 0 0 10px;
+    }
+  }
+`;
+export const Arrow = styled("div")`
+  width: auto;
+  margin: 10px;
+  .arrow {
+    font-size: ${FONT_XXL};
+    color: ${(props) => props.theme.buttonColor.primary};
+    ${TRANSITION}
+    &:hover {
+      color: ${(props) => props.theme.buttonColor.onHover};
+    }
+  }
+  .arrow__l {
+    transform: rotate(180deg);
+  }
+`;
