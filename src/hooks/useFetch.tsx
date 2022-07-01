5118c82da6a2924929b38d1148b26853da6cff69
Seheon Yu <seheon@seoultech.ac.kr>
2022-07-01 17:40:20 +0900

    feat: useFetch to fetch something from online

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export default function useFetch<DataType>(
+  url: string,
+  options: RequestInit
+): [DataType, boolean, Error | null] {
+  const [data, setData] = useState<DataType>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<any>(null);
+
+  useEffect(() => {
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.data);
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    setLoading(false);
+  }, [data, error]);
+
+  return [data, loading, error];
+}
