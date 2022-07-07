11dceaaddcf5842c4868a37a1e13d92bfdfa3bf6
Seheon Yu <seheon@seoultech.ac.kr>
2022-07-08 00:13:06 +0900

    refactor: Fancy useFetch hook

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,52 +1,108 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useTokenContext } from "../context/tokenState";
 
-type HTTPMethod = "GET" | "POST" | "PUT" | "DELETE";
+type HTTPMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
 
-interface Request<DataType> {
-  get: (path: string) => Promise<DataType>;
-  post: (path: string, body: Object) => Promise<DataType>;
-  put: (path: string, body: Object) => Promise<DataType>;
-  del: (path: string) => Promise<DataType>;
+interface Request {
+  get: <DataType>(
+    path: string
+  ) => Promise<{ error: number | undefined } & DataType>;
+  post: <DataType>(
+    path: string,
+    body: Object
+  ) => Promise<{ error: number | undefined } & DataType>;
+  put: <DataType>(
+    path: string,
+    body: Object
+  ) => Promise<{ error: number | undefined } & DataType>;
+  patch: <DataType>(
+    path: string,
+    body: Object
+  ) => Promise<{ error: number | undefined } & DataType>;
+  del: <DataType>(
+    path: string
+  ) => Promise<{ error: number | undefined } & DataType>;
 }
 
-interface Result<DataType> {
-  data: DataType | null;
+interface Result {
+  data: any;
   loading: boolean;
-  error: any;
+  error: number | null;
 }
 
-export default function useFetch<DataType>(host: string) {
-  const { accessToken } = useTokenContext();
+async function refreshJWT(
+  setToken: (accessToken: string, refreshToken: string) => void
+) {
+  const refreshToken = localStorage.getItem("refreshToken");
+  const response = await fetch("/api/refresh", {
+    method: "POST",
+    body: JSON.stringify({ refreshToken }),
+  });
+  if (response.ok) {
+    const { access_token, refresh_token } = await response.json();
+    setToken(access_token, refresh_token);
+  } else {
+    throw response.status;
+  }
+}
+
+export default function useFetch(host: string, path?: string) {
+  const { accessToken, setToken } = useTokenContext();
+  const [{ loading, data, error }, setResult] = useState<Result>({
+    data: null,
+    loading: true,
+    error: null,
+  });
 
   function makeFetch(
     method: HTTPMethod
-  ): (path: string, body?: Object) => Promise<DataType> {
-    return (path: string, body?: Object) =>
-      fetch(`${host}${path}`, {
+  ): <DataType>(
+    path: string,
+    body?: Object
+  ) => Promise<{ error: number | undefined } & DataType> {
+    return async (path: string, body?: Object) => {
+      setResult({ data: null, loading: true, error: null });
+      const res = await fetch(`${host}${path}`, {
         method,
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${accessToken}`,
         },
         body: JSON.stringify(body),
-      })
-        .then((res) => res.json())
-        .then((res) => res as DataType);
+      });
+      if (res.ok) {
+        const json = await res.json();
+        setResult({ loading: false, data: json, error: null });
+        return { ...data };
+      } else if (res.status === 401) {
+        try {
+          await refreshJWT(setToken);
+        } catch (error) {
+          if (typeof error === "number") {
+            setResult({ loading: false, data: null, error });
+          } else {
+            console.error(error);
+          }
+        }
+        return makeFetch(method)(path, body);
+      } else {
+        setResult({ loading: false, data: null, error: res.status });
+        return { error };
+      }
+    };
   }
 
-  const [{ get, post, put, del }, setRequest] = useState<Request<DataType>>({
+  const { get, post, put, patch, del }: Request = {
     get: makeFetch("GET"),
     post: makeFetch("POST"),
     put: makeFetch("PUT"),
+    patch: makeFetch("PATCH"),
     del: makeFetch("DELETE"),
-  });
+  };
 
-  const [{ loading, data, error }, setResult] = useState<Result<DataType>>({
-    data: null,
-    loading: true,
-    error: null,
-  });
+  if (path) {
+    get(path);
+  }
 
-  return { get, post, put, del, data, loading, error };
+  return { get, post, put, patch, del, data, loading, error };
 }
