0cdac8dbfd8763810e90a9fdb66d238d026c6c4c
TProgrammer <77713508+TProgramer@users.noreply.github.com>
2022-07-18 14:09:49 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 REPOSITORY=/home/ec2-user/FE
 cd $REPOSITORY
 
-sudo rm -rf node-moduels
-sudo npm install pm2 -g
-sudo npm install --legacy-peer-deps
-sudo npm install next
+#sudo rm -rf node-moduels
+#sudo npm install pm2 -g
+#sudo npm install --legacy-peer-deps
+#sudo npm install next
 # sudo npm run build
-pm2 start npm --name "dev" -- start
-pm2 list
+#pm2 start npm --name "dev" -- start
+#pm2 list
