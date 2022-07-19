8aa6b164c147003e07f34022f39652cf45b8172b
TProgrammer <77713508+TProgramer@users.noreply.github.com>
2022-07-19 23:06:21 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 REPOSITORY=/home/ubuntu/FE
 cd $REPOSITORY
-#sudo npm install --legacy-peer-deps
-#npm run build
+npm install --legacy-peer-deps
+npm run build
 #pm2 start npm --name "dev" -- start
-#pm2 reload dev
+pm2 reload dev
